beab76c0cb1629c36ca39c78144d219fec3cd052
winton <mail@wintoni.us>
2019-01-09 15:31:44 -0500

    First commit

---
+++ ad.js
@@ -0,0 +1,138 @@
+/*global Promise*/
+/*prettier-ignore*/
+"use strict";
+
+module.exports = function(dot, opts) {
+  opts = opts || {}
+
+  if (dot.state.ad) {
+    return dot
+  }
+
+  dot.state.ad = Object.assign(
+    {
+      initPromise: Promise.resolve(),
+      slots: {},
+    },
+    opts
+  )
+
+  dot.any("ad", ad)
+  dot.state.ad.initPromise.then(setupListeners.bind(dot))
+
+  return dot
+}
+
+function ad(o) {
+  var dot = o.dot,
+    opt = o.opt,
+    prop = o.prop
+
+  var promise = dot.state.ad.initPromise
+
+  if (opt.config) {
+    promise = promise.then(function() {
+      dot.set(prop, "config", opt.config)
+    })
+  }
+
+  if (opt.unit) {
+    promise = promise.then(function() {
+      unit(dot.state.ad.slots, opt.unit)
+    })
+  }
+
+  return promise
+}
+
+function setupListeners() {
+  var dot = this
+
+  window.googletag
+    .pubads()
+    .addEventListener("slotRenderEnded", function(e) {
+      dot("adRendered", {
+        divId: e.slot.getSlotElementId(),
+        isEmpty: e.isEmpty,
+        size: e.size,
+      })
+    })
+
+  window.googletag
+    .pubads()
+    .addEventListener("slotOnload", function(e) {
+      dot("adLoaded", { divId: e.slot.getSlotElementId() })
+    })
+}
+
+function unit(slots, u) {
+  var a9 = u.a9,
+    destroy = u.destroy,
+    divId = u.divId,
+    oop = u.oop,
+    path = u.path,
+    refresh = u.refresh,
+    sizeMap = u.sizeMap,
+    sizes = u.sizes,
+    targets = u.targets
+
+  var promise, slot
+
+  if (destroy) {
+    window.googletag.destroySlots(slots[divId])
+  } else if (refresh) {
+    window.googletag.pubads().refresh([slots[divId]])
+  } else if (oop) {
+    slot = window.googletag.defineOutOfPageSlot(path, divId)
+  } else {
+    slot = window.googletag.defineSlot(path, sizes, divId)
+
+    if (slot && sizeMap) {
+      slot.defineSizeMapping(sizeMap)
+    }
+  }
+
+  if (slot && targets) {
+    for (var key in targets) {
+      slot.setTargeting(key, targets[key])
+    }
+  }
+
+  if (slot && a9) {
+    promise = fetchA9(u)
+  }
+
+  if (slot) {
+    slot.addService(window.googletag.pubads())
+    window.googletag.display(divId)
+  }
+
+  slots[divId] = slot
+
+  return promise
+}
+
+function fetchA9(u) {
+  var divId = u.divId,
+    path = u.path,
+    sizes = u.sizes
+
+  return new Promise(function(resolve) {
+    window.apstag.fetchBids(
+      {
+        slots: [
+          {
+            sizes: sizes,
+            slotID: divId,
+            slotName: path,
+          },
+        ],
+        timeout: 2e3,
+      },
+      function() {
+        window.apstag.setDisplayBids()
+        resolve()
+      }
+    )
+  })
+}
---
+++ adTest.js
@@ -0,0 +1,35 @@
+/*global Promise*/
+/*prettier-ignore*/
+"use strict";
+
+var ad = require("./ad"),
+  dot = require("dot-event")()
+
+var initPromise = new Promise(function(resolve) {
+  window.googletag.cmd.push(function() {
+    window.googletag
+      .pubads()
+      .setCategoryExclusion("nonendemic")
+    window.googletag.pubads().enableSingleRequest()
+    window.googletag.pubads().collapseEmptyDivs()
+    window.googletag.pubads().setCentering(true)
+    window.googletag.enableServices()
+    resolve()
+  })
+})
+
+ad(dot, { initPromise: initPromise })
+
+dot("ad", {
+  unit: {
+    divId: "test",
+    path: "/21054826/content1_1/art_science-and-chill",
+    sizes: [300, 250],
+  },
+})
+
+// eslint-disable-next-line no-console
+dot.on("adLoaded", console.log)
+
+// eslint-disable-next-line no-console
+dot.on("adRendered", console.log)
